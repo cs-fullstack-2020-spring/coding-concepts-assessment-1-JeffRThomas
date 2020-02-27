fa4187f49c52bff98c5e597448bed47e38f00adf
JeffRThomas <thomas.jeff@live.com>
2020-02-27 13:05:21 -0600

    CopiedFiles

---
+++ YouGotThis.js
@@ -0,0 +1,39 @@
+// // JavaScript Link Check
+// // console.log("JS Linked");
+
+// //Initialize empty variables to start While Loops
+// let usersResponses = [];
+// let usersName = "";
+// let usersEmail = "";
+// let usersMessage = "";
+
+// // Run loop to get the user's first & last name 
+// while (usersName == "") {
+//     usersName = prompt(`May I have your first and last name?\nExample: "First [Space] Last"\n*Please Do Not Leave Blank*`);
+// }
+// usersResponses.push(usersName);//Add user input to array
+// console.log(usersResponses);//Checkpoint 1
+// // Run loop to get the user's email 
+// while (usersEmail == "") {
+//     usersEmail = prompt(`May I have your email address?\nExample: "emailname@account.com"\n*Please Do Not Leave Blank*`);
+// }
+// usersResponses.push(usersEmail);//Add user input to array
+// console.log(usersResponses);//Checkpoint 2
+// // Run loop to get the user's message 
+// while (usersMessage == "") {
+//     usersMessage = prompt(`Please type your message here.\nExample: "Whatever you choose to say is fine. Click "OK" when finised."\n*Please Do Not Leave Blank*`);
+// }
+// usersResponses.push(usersMessage);//Add user input to array
+// console.log(usersResponses);//Checkpoint 3
+// // Confirm that user still wants to reach out, and verify current user input
+// let usersOk = confirm(`Are you sure you want to contact me? If not click "Cancel", otherwise click "OK" to send your message.\n
+// ${usersName}\n${usersEmail}\n"${usersMessage}"`);
+// if (usersOk==true){
+//     alert(`Thanks for stopping by! I'll reply back at my earliest convienence. Till then, HAVE A GREAT DAY!`);
+// } else {
+//     alert(`Maybe next time, thanks for stopping by.`);
+// }
+// console.log("Program Completed :-)");//Final Checkpoint "End of Program"
+
+
+
